eef9139df5d4d78c03053cf7f1aca9218be5170f
tapokVozmezdia <112655065+tapokVozmezdia@users.noreply.github.com>
2022-10-22 22:12:58 +0300

    Create main.c
    Файл с программным кодом первой лабораторной работы по ПЗРЛ. Забора Георгий Данилович, КМБО-02-22, 2-ая подгруппа.

---
+++ main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+void selectionSort(int size, float *array)
+{
+        float min = pow(2,31) + 1;
+        int minInd;
+        float assist;
+        int j;
+        for(int i = 0; i < size; ++i) {
+            minInd = i;
+            min = array[i];
+            for(j = i; j < size; ++j) {
+                if (array[j] < min) {
+                    minInd = j;
+                    min = array[j];
+                }
+            }
+            assist = array[minInd];
+            array[minInd] = array[i];
+            array[i] = assist;
+        }
+}
+
+int main()
+{
+        int numOfElements;
+        int inputGiven = 0;
+        do {
+            scanf("%d", &numOfElements);
+            if (numOfElements <= 0) {
+                printf("Invalid input. Number of elements must be positive.\n");
+            }
+            else {
+                inputGiven = 1;
+            }
+        }
+        while (inputGiven == 0);
+        float *array = (float*) malloc(sizeof(float) * numOfElements);
+        for (int counter = 0; counter < numOfElements; ++counter) {
+            scanf("%f", &array[counter]);
+        }
+        selectionSort(numOfElements, array);
+        for(int counter0 = 0; counter0 < numOfElements; counter0++){
+            printf("%.2f ", array[counter0]);
+        }
+        free(array);
+        return 0;
+}
